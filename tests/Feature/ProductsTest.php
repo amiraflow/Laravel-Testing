a4a7b4e9f3eca02b0cee9868f6a572d215ed6a39
amiraflow <amira.midzic@gmail.com>
2022-08-14 21:45:51 +0200

    DRY principle using setUp function

--- tests/Feature/ProductsTest.php
+++ tests/Feature/ProductsTest.php
@@ -13,10 +13,19 @@ class ProductsTest extends TestCase
 
     use RefreshDatabase; // run migrations on the temporary sqlite database from phpunit.xml
 
+    private User $user;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = $this->createUser();
+
+    }
+
     public function test_homepage_contains_empty_table()
     {
-        $user = User::factory()->create();
-        $response = $this->actingAs($user)->get('/products');
+        $response = $this->actingAs($this->user)->get('/products');
 
         $response->assertStatus(200);
         $response->assertSee(__('No products found') );
@@ -24,13 +33,12 @@ class ProductsTest extends TestCase
 
     public function test_homepage_contains_non_empty_table()
     {
-        $user = User::factory()->create();
         $product = Product::create([
             'name' => 'Product 1',
             'price' => 123
         ]);
         $response = $this->get('/products');
-        $response = $this->actingAs($user)->get('/products');
+        $response = $this->actingAs($this->user)->get('/products');
 
         $response->assertStatus(200);
         $response->assertDontSee(__('No products found') );
@@ -42,15 +50,19 @@ class ProductsTest extends TestCase
 
     public function test_paginated_products_table_doesnt_contain_11th_record()
     {
-        $user = User::factory()->create();
         $products = Product::factory(11)->create();
         $lastProduct = $products->last();
 
-        $response = $this->actingAs($user)->get('/products');
+        $response = $this->actingAs($this->user)->get('/products');
 
         $response->assertStatus(200);
         $response->assertViewHas('products', function ($collection) use ($lastProduct) {
             return !$collection->contains($lastProduct);
         });
+    }
+
+    private function createUser(): User 
+    {
+        return User::factory()->create();
     }
 }
