47cdb0b0b9632aaa48fd569f26cc29eb6fac2c35
amiraflow <amira.midzic@gmail.com>
2022-08-15 14:07:53 +0200

    Delete product test

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -52,6 +52,11 @@
                                             <a href="{{ route('products.edit', $product) }}" class="mb-4 inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
                                                 Edit</a>
                                         </td>
+                                        <form action="{{ route('products.destroy', $product) }}" method="POST" class="inline-block">
+                                                @csrf
+                                                @method('DELETE')
+                                                <x-button onclick="return confirm('Are you sure?')" class="bg-red-600 text-white">Delete</x-button>
+                                            </form>
                                     @endif
                                 </tr>
                             @empty
